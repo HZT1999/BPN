f10fb029e16c147b30a1b5737e3489331055dd42
HZT1999 <zac@hztechnologies.com.au>
2019-06-03 19:34:57 +1000

    Second Commit

---
+++ bitcoin_price_notifier.py
@@ -0,0 +1,8 @@
+from pip._vendor import requests
+
+bitcoin_api_url = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/'
+response = requests.get(bitcoin_api_url)
+response_json = response.json()
+type(response_json)
+
+response_json[0]
